7288b7512eff373b6ab52816c4d6717208e0bff2
Ashwagandhae <julianlianbauer@gmail.com>
2025-12-21 15:53:42 +0100

    update setup

--- scripts/setup-typst.ts
+++ scripts/setup-typst.ts
@@ -1,29 +1,61 @@
+// scripts/setup-typst.js
 import fs from "fs";
 import { execSync } from "child_process";
+import { pipeline } from "stream/promises";
 
 const VERSION = "v0.14.2";
-const FILENAME = `typst-x86_64-unknown-linux-musl.tar.xz`;
+const ARCH = "x86_64-unknown-linux-musl";
+const FILENAME = `typst-${ARCH}.tar.xz`;
 const URL = `https://github.com/typst/typst/releases/download/${VERSION}/${FILENAME}`;
 
-function install() {
+async function install() {
   if (process.platform !== "linux") {
-    console.log("skipping typst download (not linux)");
+    console.log("skipping typst download (not Linux)");
     return;
   }
 
-  console.log(`downloading typst ${VERSION}...`);
+  console.log(`downloading Typst ${VERSION} from:`);
+  console.log(URL);
 
-  execSync(`curl -L -o typst.tar.xz ${URL}`);
+  const response = await fetch(URL);
 
-  execSync("tar -xf typst.tar.xz");
+  if (!response.ok) {
+    throw new Error(
+      `failed to download: ${response.status} ${response.statusText}`
+    );
+  }
+  if (response.body == null) {
+    throw new Error("response body is null");
+  }
 
-  const folderName = FILENAME.replace(".tar.xz", "");
+  const fileStream = fs.createWriteStream("typst.tar.xz");
+
+  await pipeline(response.body, fileStream);
+
+  console.log("download complete. extracting...");
+
+  try {
+    execSync("tar -xf typst.tar.xz");
+  } catch (e) {
+    console.error("tar extraction failed.");
+    throw e;
+  }
+
+  const folderName = `typst-${ARCH}`;
+
+  if (fs.existsSync("typst")) fs.unlinkSync("typst");
+
   fs.renameSync(`${folderName}/typst`, "./typst");
 
-  fs.rmSync("typst.tar.xz");
+  fs.unlinkSync("typst.tar.xz");
   fs.rmSync(folderName, { recursive: true, force: true });
 
-  console.log("yypst installed successfully.");
+  fs.chmodSync("./typst", "755");
+
+  console.log("typst installed successfully.");
 }
 
-install();
+install().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
